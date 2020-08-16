dbf9606a4d653e2f880b3a9e8c71fee9f33b81ab
Shawn Presser <shawnpresser@gmail.com>
2020-08-15 20:10:20 -0700

    WIP nflex / mel

--- nflex.py
+++ nflex.py
@@ -1,10 +1,14 @@
 import numpy as np
+import mel_common
 
 def val(a):
-  return np.array(a)
+  #return np.array(a)
+  #return mel_common.cast(a)
+  return mel_common.const(a)
 
 def shape(a):
-  return list(val(a).shape)
+  #return list(val(a).shape)
+  return mel_common.shaped(a)
 
 def rank(a):
   return len(shape(a))
@@ -17,10 +21,13 @@ def transposed(a, axis=-2):
 
 def grow_along_last_axis(a, n, initial_value=0):
   a = val(a)
-  value = np.zeros(shape(a)[0:-1] + [n], dtype=a.dtype)
+  #value = np.zeros(shape(a)[0:-1] + [n], dtype=a.dtype)
+  value = mel_common.zeros(shape(a)[0:-1] + [n], dtype=a.dtype)
   value.fill(initial_value)
-  a1 = np.append(a, value, axis=-1)
-  return a1.astype(a.dtype)
+  #a1 = np.append(a, value, axis=-1)
+  #a1 = np.concatenate([a, value], axis=-1)
+  #return a1.astype(a.dtype)
+  return mel_common.append(a, value, axis=-1)
 
 def grow_along_axis(a, n, axis=-1, initial_value=0):
   if axis == -1 or axis == shape(a)[-1]:
@@ -35,17 +42,17 @@ from functools import partial
 grow_along_rows = partial(grow_along_axis, axis=-1)
 grow_along_cols = partial(grow_along_axis, axis=-2)
 
-def get_padding(size, alignment):
+def padding(size, alignment):
   assert alignment > 0
   return -size % alignment
 
-def get_padded_size(size, alignment):
-  return size + get_padding(size, alignment)
+def aligned(size, alignment):
+  return size + padding(size, alignment)
 
 def pad_along_axis(a, n, axis=-1, initial_value=0):
   a = val(a)
-  padding = get_padding(shape(a)[axis], n)
-  return grow_along_axis(a, padding, axis=axis, initial_value=initial_value)
+  padding_ = padding(shape(a)[axis], n)
+  return grow_along_axis(a, padding_, axis=axis, initial_value=initial_value)
 
 pad_along_rows = partial(pad_along_axis, axis=-1)
 pad_along_cols = partial(pad_along_axis, axis=-2)
